c485fdb59672f77d1b1fdd02cbc4a7c4bc2088ba
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-10-23 15:22:30 +0100

    components added

---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(props) {
+  return (
+    <div className="footer">
+      <h3>Sarah Barron</h3>
+    </div>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header(props) {
+  return (
+    <div className="header">
+      <h1>Gallery of Horns</h1>
+    </div>
+  );
+}
